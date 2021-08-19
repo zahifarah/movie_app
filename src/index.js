4c7fec79427dedac4f15776f102ff6583623c698
Zahi Farah <zahi.farah@protonmail.com>
2021-08-19 16:51:41 +0300

    Simplify ReactDOM.render as much as possible.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+// this is what a component looks like when you want to use it.
+ReactDOM.render(<App/>, document.getElementById("root")) // it is passed as JSX element
+
+// React can only render one application.
