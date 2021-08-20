e1b93dca0156cc776f7b98e2bd6828565801f9a7
Zahi Farah <zahi.farah@protonmail.com>
2021-08-20 10:07:43 +0300

    Refactor code.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import PropTypes from "prop-types";
+import { thisStringValue } from 'es-abstract';
 
-// the App component is the only one we're rendering (in "index.js")
+class App extends React.Component {
+    state = {
+        count: 0
+    };
+    
+    add = () => {
+        this.setState(current => ({count: current.count + 1}));
+    };
+    remove = () => {
+        this.setState(current => ({count: current.count -1 }));
+    };
 
-
-/** we can create components differently, breaking down and calling them in App component */
-// function renderFood(dish) {
-//     return <Food name={dish.name} picture={dish.image} />
-// }
+    render() {
+        return (
+        <div>
+            <h1>The number is: {this.state.count}</h1>
+            <button onClick={this.add}> Add </button>
+            <button onClick={this.remove}> Remove </button>
+        </div>
+        )
+    }
+}
 
 // export to use, default is "index.js"
 export default App;
