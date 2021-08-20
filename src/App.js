127f1bcc381fbf1df27cd6d94cfa221f7645296d
Zahi Farah <zahi.farah@protonmail.com>
2021-08-20 11:04:40 +0300

    Loading/Not Loading exercise.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 class App extends React.Component {
     state = {
-        count: 0
+        isLoading: true,
+        movies: [],
     };
-    
-    add = () => {
-        this.setState(current => ({count: current.count + 1}));
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({isLoading: false})
+        }, 3000);
     };
-    remove = () => {
-        this.setState(current => ({count: current.count -1 }));
-    };
-
     render() {
-        return (
-        <div>
-            <h1>The number is: {this.state.count}</h1>
-            <button onClick={this.add}> Add </button>
-            <button onClick={this.remove}> Remove </button>
-        </div>
-        )
-    }
-}
+        const {isLoading} = this.state;
+        return <div>{ isLoading ? "Loading..." : "NOT loading" }</div>
+    };
+};
 
 // export to use, default is "index.js"
 export default App;
