6969493976c3129ed759b01ebcd06bc90c597d1b
Zahi Farah <zahi.farah@protonmail.com>
2021-08-21 18:00:41 +0300

    Turn component into <Link>, and pass it a *pathname* and *state* field.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,26 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import { Link } from "react-router-dom";
 
 function Movie({ year, title, summary, poster, genres }) {
    return (
-      <div className="movie">
-         <img src={poster} alt={title} title={title} />
-         <div className="movie__data">
-            <h3 className="movie__title">
-               {title} <span className="movie__year">({year})</span>
-            </h3>
-            {/* <h4 className="movie__year">{year}</h4> */}
-            <ul className="movie__genres">
-               {genres.map((genre, index) => (
-                  <li key={index} className="genres_genre">
-                     {genre}
-                  </li>
-               ))}
-            </ul>
-            <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      <Link
+         to={{
+            pathname: "/movie-detail",
+            state: {
+               year: year, // state: prop
+               title: title,
+               summary,
+               poster,
+               genres,
+            },
+         }}
+      >
+         <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+               <h3 className="movie__title">
+                  {title} <span className="movie__year">({year})</span>
+               </h3>
+               {/* <h4 className="movie__year">{year}</h4> */}
+               <ul className="movie__genres">
+                  {genres.map((genre, index) => (
+                     <li key={index} className="genres_genre">
+                        {genre}
+                     </li>
+                  ))}
+               </ul>
+               <p className="movie__summary">{summary.slice(0, 180)}...</p>
+            </div>
          </div>
-      </div>
+      </Link>
    );
 }
 
