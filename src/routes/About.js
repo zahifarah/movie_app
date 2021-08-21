86f2eb6446e80cb4a7ea68628945935add8e35a6
Zahi Farah <zahi.farah@protonmail.com>
2021-08-21 17:59:17 +0300

    Print props.

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function About() {
+function About(props) {
+   console.log(props);
    return (
       <span>
          About Page: I built it because I love movies and am learning React :)
