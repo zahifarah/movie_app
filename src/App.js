064628c78c5a4db20eeee16112dc29ead7711755
Zahi Farah <zahi.farah@protonmail.com>
2021-08-21 10:19:51 +0300

    Styling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
    state = {
@@ -26,22 +27,29 @@ class App extends React.Component {
    render() {
       const { isLoading, movies } = this.state;
       return (
-         <div>
-            {isLoading
-               ? "Loading..."
-               : movies.map((movie) => {
-                    return (
-                       <Movie
-                          key={movie.id}
-                          id={movie.id}
-                          title={movie.title}
-                          year={movie.year}
-                          summary={movie.summary}
-                          poster={movie.medium_cover_image}
-                       />
-                    );
-                 })}
-         </div>
+         <section className="container">
+            {isLoading ? (
+               <div className="loader">
+                  <span className="loader__text">Loading...</span>
+               </div>
+            ) : (
+               <div className="movies">
+                  {movies.map((movie) => {
+                     return (
+                        <Movie
+                           key={movie.id}
+                           id={movie.id}
+                           title={movie.title}
+                           year={movie.year}
+                           summary={movie.summary}
+                           poster={movie.medium_cover_image}
+                           genres={movie.genres}
+                        />
+                     );
+                  })}
+               </div>
+            )}
+         </section>
       );
    }
 }
