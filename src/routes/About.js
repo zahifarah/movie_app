9759d69f3b9b0bd65dd21568a97b95b03de175aa
Zahi Farah <zahi.farah@protonmail.com>
2021-08-21 12:54:57 +0300

    Basic About component.

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+   return (
+      <span>
+         About Page: I built it because I love movies and am learning React :)
+      </span>
+   );
+}
+
+export default About;
